82da4f76f15541de5146ad6fd1b87c9d09896008
roryrmd <69695788+roryrmd@users.noreply.github.com>
2021-08-04 22:49:45 +0700

    edit backend

--- src/main/java/mcc53/com/models/Project.java
+++ src/main/java/mcc53/com/models/Project.java
@@ -12,6 +12,8 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name = "project")
